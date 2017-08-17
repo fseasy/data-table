3ae6a6a96d9c6e520c6ebe45ff00575822b6b340
memeda <readonlyfile@hotmail.com>
2017-08-17 23:57:24 +0800

    OH! seem to solve the key point

--- data_table.cpp
+++ data_table.cpp
@@ -6,35 +6,80 @@ using namespace std;
 
 class FieldInfoBase
 {
-    virtual ~FieldInfoBase(){};
+    public:
+        virtual ~FieldInfoBase(){};
 };
 
-template<typename T>
+template<typename T, typename FuncT>
 class FieldInfo: public FieldInfoBase
 {
     public:
         using FieldType = T;
-        using FieldParseFuncT = function<T(const string&)>;
+        using FieldParseFuncT = FuncT;
     public:
-        FieldInfo(const function<T(const string&)>& parse_func):
+        FieldInfo(const FuncT& parse_func):
             parse_func_(parse_func)
         {}
+        FuncT* get_parse_func(){ return &parse_func; }
 
     private:
-        FieldParseFuncT parse_func_;
+        FuncT parse_func_;
 };
 
-template <typename T>
-FieldInfoBase* make_field(function<T(const string&)> func)
+template <typename T, typename FuncT>
+FieldInfoBase* make_field(const FuncT& func)
 {
-    FieldInfoBase * field_ptr = new FieldInfo<T>(func);
+    FieldInfoBase * field_ptr = new FieldInfo<T, FuncT>(func);
     return field_ptr;
 }
+
+class FieldData
+{
+    public:
+        FieldData(const string& value_in_str,
+                  shared_ptr<FieldInfoBase> field_info)
+            : value_in_str(value_in_str),
+              p_field_info(field_info)
+        {}
+        template <typename T>
+        bool parse(T& value)
+        { 
+            return p_field_info->get_parse_func()(value_in_str, value);
+        };
+    private:
+        string value_in_str;
+        shared_ptr<FieldInfoBase> p_field_info;
+};
+
+class RawData
+{
+    public:
+        RawData(const vector<string>& field_str_repr_list,
+                const vector<shared_ptr<FieldInfoBase>>& field_info_list)
+        {
+            assert(field_str_repr_list.size() ==
+                   field_info_list.size());
+            for(size_t i = 0U; i < field_info_list.size(); ++i)
+            {
+                field_data_list.emplace_back(field_str_repr_list[i],
+                                             field_info_list[i]);
+            }
+        }
+        template<typename T>
+        bool get(unsigned index, T& value)
+        {
+            return field_data_list[index].parse(value);
+        }
+    private:
+        vector<FieldData> field_data_list;
+};
 
 class FieldInitHelper
 {
     public:
-        FieldInitHelper(DataTable* data_table_ptr);
+        FieldInitHelper(DataTable* data_table_ptr):
+            data_table_ptr(data_table_ptr)
+    {};
     public:
         FieldInitHelper& operator(FieldInfoBase* field_base)
         {
@@ -49,16 +94,42 @@ class FieldInitHelper
 class DataTable
 {
     public:
+        DataTable(istream& is, const string& field_delimiter,
+                  bool close_istream_when_deconstruction=true)
+            :is(is),
+             field_delimieter(field_delimieter),
+             close_istream_when_deconstruction(close_istream_when_deconstruction)
+        {}
+    public:
+        DataTable& get_next_row(RawData &row, bool &is_row_ok)
+        {
+            
+        }
+    public:
         FieldInitHelper add_fields()
         {
             return FieldInitHelper(this);
-
         }
         void add(shared_ptr<FieldInfoBase> field_info)
         {
             field_info_list.push_back(field_info);
         }
     private:
+        bool split_line2fields(const string& line, vector<string>& field_str_repr_list)
+        {
+            using std::swap;
+            vector<string> fields_tmp(field_info_list.size());
+            size_t start_pos = 0u;
+            for(size_t i = 0u; i < field_info_list.size(); ++i)
+            {
+                size_t delim_pos = line.find(field_delimieter, start_pos);
+                // TODO
+            }
+        }
+    private:
         vector<shared_ptr<FieldInfoBase>> field_info_list;
+        istream is;
+        string field_delimiter;
+        bool close_istream_when_deconstruction;
 };
 
